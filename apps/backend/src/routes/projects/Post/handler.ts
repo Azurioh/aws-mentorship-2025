9e5cc2e91528603e13e6772a8ee00e33268b8740
Alan CUNIN <alan.cunin@epitech.eu>
2025-07-26 16:15:39 +0200

    feat(backend): add the create project route

--- apps/backend/src/routes/projects/Post/handler.ts
+++ apps/backend/src/routes/projects/Post/handler.ts
@@ -1,16 +1,23 @@
-import type { FastifyReply, FastifyRequest } from "fastify";
-import { TBody, THeaders, TParams, TQuerystring } from "./schema";
+import type { FastifyReply, FastifyRequest } from 'fastify';
+import { Body, BodyToData, type TBody, type THeaders, type TParams, type TQuerystring } from './schema';
+import { HttpStatusCode } from '@test-connect/shared/enums/http-status';
+import { createProject } from './db-access';
 
 const handler = async (
-    req: FastifyRequest<{
-        Body: TBody;
-        Headers: THeaders;
-        Params: TParams;
-        Querystring: TQuerystring;
-    }>,
-    res: FastifyReply
+  req: FastifyRequest<{
+    Body: TBody;
+    Headers: THeaders;
+    Params: TParams;
+    Querystring: TQuerystring;
+  }>,
+  res: FastifyReply,
 ) => {
-    return res.status(200).send({ msg: "yes" });
+  const body = Body.parse(req.body);
+  const data = BodyToData(body);
+
+  const project = await createProject(data);
+
+  return res.success(project, HttpStatusCode.created);
 };
 
 export default handler;
