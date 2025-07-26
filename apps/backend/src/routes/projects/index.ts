1b4c4cb6e5f3b56c9d574fa72b814374db0b2921
Alan CUNIN <alan.cunin@epitech.eu>
2025-07-26 16:31:55 +0200

    feat(backend): add the list projects route

--- apps/backend/src/routes/projects/index.ts
+++ apps/backend/src/routes/projects/index.ts
@@ -1,6 +1,8 @@
 import type { FastifyInstance } from 'fastify';
 import CreateProject from './create-project';
+import ListProjects from './list-projects';
 
 export default (app: FastifyInstance) => {
   CreateProject(app);
+  ListProjects(app);
 };
