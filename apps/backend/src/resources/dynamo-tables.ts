ded4c8f0c32aaca72a4308b301fb10706e36e859
Alan CUNIN <alan.cunin@epitech.eu>
2025-08-07 10:34:15 +0200

    refactor(backend): update the project dynamo table

--- apps/backend/src/resources/dynamo-tables.ts
+++ apps/backend/src/resources/dynamo-tables.ts
@@ -2,14 +2,23 @@ export default {
   ProjectsTable: {
     Type: 'AWS::DynamoDB::Table',
     Properties: {
-      TableName: "${self:provider.environment.PROJECTS_TABLE}-${self:provider.stage}",
+      TableName: '${self:provider.environment.PROJECTS_TABLE}-${self:provider.stage}',
       AttributeDefinitions: [
-        { AttributeName: 'userId', AttributeType: 'S' },
+        { AttributeName: 'ownerId', AttributeType: 'S' },
         { AttributeName: 'id', AttributeType: 'S' },
       ],
       KeySchema: [
-        { AttributeName: 'userId', KeyType: 'HASH' },
+        { AttributeName: 'ownerId', KeyType: 'HASH' },
         { AttributeName: 'id', KeyType: 'RANGE' },
+      ],
+      GlobalSecondaryIndexes: [
+        {
+          IndexName: 'ProjectIdIndex',
+          KeySchema: [{ AttributeName: 'id', KeyType: 'HASH' }],
+          Projection: {
+            ProjectionType: 'ALL',
+          },
+        },
       ],
       BillingMode: 'PAY_PER_REQUEST',
     },
