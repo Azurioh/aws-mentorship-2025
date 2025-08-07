6a7b40c3cf1ed568a2f3aaa8f438efba2485b3ef
Alan CUNIN <alan.cunin@epitech.eu>
2025-08-07 10:37:31 +0200

    style(backend): lint the core/app file

--- apps/backend/src/core/app.ts
+++ apps/backend/src/core/app.ts
@@ -1,83 +1,81 @@
-import { setupCors } from "@config/cors";
-import { setupDecorators } from "@config/decorators/setupDecorators";
-import { setupRateLimit } from "@config/rate-limit";
-import { setupSwagger } from "@config/swagger";
-import { router as apiRoutes } from "@routes";
-import Ajv from "ajv";
-import ajvFormats from "ajv-formats";
-import fastify, { type FastifyInstance } from "fastify";
-import { errorHandler, notFoundHandler } from "@config/error-handler";
+import { setupCors } from '@config/cors';
+import { setupDecorators } from '@config/decorators/setupDecorators';
+import { setupRateLimit } from '@config/rate-limit';
+import { setupSwagger } from '@config/swagger';
+import { router as apiRoutes } from '@routes';
+import Ajv from 'ajv';
+import ajvFormats from 'ajv-formats';
+import fastify, { type FastifyInstance } from 'fastify';
+import { errorHandler, notFoundHandler } from '@config/error-handler';
 import { environment } from '@config/environment';
-import { createRemoteJWKSet, jwtVerify } from 'jose'
+import { createRemoteJWKSet } from 'jose';
 
-const COGNITO_ISSUER = `https://cognito-idp.eu-west-3.amazonaws.com/${environment.COGNITO_ID}`
-const JWKS = createRemoteJWKSet(new URL(`${COGNITO_ISSUER}/.well-known/jwks.json`))
+const COGNITO_ISSUER = `https://cognito-idp.eu-west-3.amazonaws.com/${environment.COGNITO_ID}`;
+const JWKS = createRemoteJWKSet(new URL(`${COGNITO_ISSUER}/.well-known/jwks.json`));
 
 const build = (): FastifyInstance => {
-    const app = fastify({
-        bodyLimit: 10 * 1024 * 1024,
-        ignoreTrailingSlash: true,
-    });
+  const app = fastify({
+    bodyLimit: 10 * 1024 * 1024,
+    ignoreTrailingSlash: true,
+  });
 
-    /*!> Setup decorators */
-    setupDecorators(app);
+  /*!> Setup decorators */
+  setupDecorators(app);
 
-   
+  app.get('/docs-check', async (req, res) => {
+    console.log(JSON.stringify(req));
+    return { status: 'ok' };
+  });
 
-    app.get("/docs-check", async (req, res) => {
-        console.log(JSON.stringify(req))
-        return { status: "ok" };
-    });
+  /*!> Setup Fastify plugins */
+  setupCors(app);
+  setupRateLimit(app);
 
-    /*!> Setup Fastify plugins */
-    setupCors(app);
-    setupRateLimit(app);
+  if (environment.NODE_ENV && environment.NODE_ENV !== 'production') {
+    setupSwagger(app);
+  }
 
-    if (environment.NODE_ENV && environment.NODE_ENV !== 'production'){
-        setupSwagger(app);
+  /*!> Setup the routers */
+  // app.register(apiRoutes);
+
+  app.addHook('onRequest', async (req, res) => {
+    const authHeader = req.headers.authorization;
+
+    if (!authHeader || !authHeader.startsWith('Bearer ')) {
+      res.code(401).send({ message: 'Unauthorized' });
+      return;
     }
 
-    /*!> Setup the routers */
-    // app.register(apiRoutes);
+    const token = authHeader.substring('Bearer '.length);
 
-    app.addHook('onRequest', async (req, res) => {
-        const authHeader = req.headers.authorization;
+    // Ensuite tu peux faire la validation de ton JWT
+    try {
+      const { payload } = await jwtVerify(token, JWKS, {
+        issuer: COGNITO_ISSUER,
+        algorithms: ['RS256'],
+      });
+      req.user = payload;
+    } catch (err) {
+      res.code(401).send({ message: 'Invalid token' });
+    }
+  });
 
-        if (!authHeader || !authHeader.startsWith('Bearer ')) {
-            res.code(401).send({ message: 'Unauthorized' });
-            return;
-        }
+  apiRoutes(app);
 
-        const token = authHeader.substring('Bearer '.length);
+  /*!> Register the not found error handler */
+  app.setNotFoundHandler(notFoundHandler);
 
-        // Ensuite tu peux faire la validation de ton JWT
-        try {
-            const { payload } = await jwtVerify(token, JWKS, {
-            issuer: COGNITO_ISSUER,
-            algorithms: ['RS256'],
-            })
-            req.user = payload
-        } catch (err) {
-            res.code(401).send({ message: 'Invalid token' });
-        }
-    })
+  /*!> Register the default error handler */
+  app.setErrorHandler(errorHandler);
 
-    apiRoutes(app);
+  /*!> Validator for request body schemas */
+  app.setValidatorCompiler(({ schema }) => {
+    const ajv = new Ajv({ coerceTypes: false, strict: true });
+    ajvFormats(ajv);
+    return ajv.compile(schema);
+  });
 
-    /*!> Register the not found error handler */
-    app.setNotFoundHandler(notFoundHandler);
-
-    /*!> Register the default error handler */
-    app.setErrorHandler(errorHandler);
-
-    /*!> Validator for request body schemas */
-    app.setValidatorCompiler(({ schema }) => {
-        const ajv = new Ajv({ coerceTypes: false, strict: true });
-        ajvFormats(ajv);
-        return ajv.compile(schema);
-    });
-
-    return app;
+  return app;
 };
 
 export default build;
